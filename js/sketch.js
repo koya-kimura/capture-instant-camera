94b69ec7b58cb9951bd4a295c10e7d423a2f3b52
koya-kimura <2ktmnri8@gmail.com>
2024-02-08 10:38:54 +0900

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,116 @@
+let mode = "ready"
+let photo;
+let capture;
+let captureTexture;
+let memoryTexture;
+let backgroundTexture;
+
+let t = 0;
+
+let filterShader;
+let backgroundFilterShader;
+
+function preload() {
+    filterShader = loadShader("../shader/main.vert", "../shader/main.frag");
+    backgroundFilterShader = loadShader("../shader/main.vert", "../shader/bg.frag");
+}
+
+function setup() {
+    createCanvas(1200, 675);
+
+    capture = createCapture(VIDEO);
+    capture.hide();
+
+    captureTexture = createGraphics(46 * 5, 62 * 5, WEBGL);
+    memoryTexture = createGraphics(width, height);
+    backgroundTexture = createGraphics(width, height, WEBGL);
+
+    photo = new Photo(270, 430);
+}
+
+function draw() {
+    background(0);
+
+    if (t < 1) {
+        captureTexture.shader(filterShader);
+
+        filterShader.setUniform("u_tex", memoryTexture);
+        filterShader.setUniform("u_time", t);
+
+        captureTexture.rect(0, 0, width, height);
+    }
+
+    if (mode == "ready") {
+        backgroundTexture.shader(backgroundFilterShader);
+        backgroundFilterShader.setUniform("u_tex", capture);
+        backgroundTexture.rect(width, height);
+
+        image(backgroundTexture, width / 2, height / 2, width, height);
+        photo.display(captureTexture);
+        instantCamera(width / 2, height / 2, 500);
+        memoryTexture.image(capture, 0, 0, width, height);
+    } else if (mode == "developing") {
+        photo.display(captureTexture);
+        t += 0.001;
+    }
+}
+
+
+function mouseClicked() {
+    if (mode == "ready") {
+        mode = "developing";
+    }
+}
+
+
+class Photo {
+    constructor(w, h) {
+        this.p = createVector(width / 2, height / 2);
+        this.v = createVector(0, -1);
+        this.scl = 0.5;
+        this.w = w;
+        this.h = h;
+        this.scl = 1;
+        this.t = 0;
+        this.pg = createGraphics(w, h);
+    }
+
+    display(captureTexture) {
+        this.pg.background(255);
+
+        this.pg.imageMode(CENTER);
+        this.pg.image(captureTexture, this.pg.width / 2, this.pg.height / 2 - 20)
+
+        imageMode(CENTER);
+        image(this.pg, this.p.x, this.p.y, this.w * this.scl, this.h * this.scl);
+    }
+}
+
+function instantCamera(x, y, s) {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill("#A2A19D")
+    rect(x, y, s * 0.8, s, s * 0.05, s * 0.05);
+    fill("#28272A")
+    rect(x + s * 0.05, y, s * 0.6, s);
+
+    fill("#F2F2F2")
+    rect(x - s * 0.25, y - s * 0.35, s * 0.1, s * 0.2, s * 0.01, s * 0.01);
+
+    strokeWeight(s * 0.03);
+    stroke("#28272A");
+    fill("#A2A19D");
+    circle(x + s * 0.05, y + s * 0.05, s * 0.7);
+
+    noStroke();
+    fill("#28272A");
+    circle(x + s * 0.05, y + s * 0.05, s * 0.5);
+    fill("#131313");
+    circle(x + s * 0.05, y + s * 0.05, s * 0.2);
+
+    fill("#B2B2B2");
+    circle(x + s * 0.07, y + s * 0.01, s * 0.05);
+    circle(x + s * 0.03, y + s * 0.07, s * 0.04);
+    pop();
+}
---
+++ shader/bg.frag
@@ -0,0 +1,42 @@
+precision mediump float;
+
+varying vec2 vTexCoord;
+
+uniform sampler2D u_tex;
+
+float PI=3.14159265358979;
+
+float random(vec2 st){
+    return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
+}
+
+mat2 rot(float angle){
+    return mat2(cos(angle),-sin(angle),sin(angle),cos(angle));
+}
+
+float atan2(float y,float x){
+    return x==0.?sign(y)*PI/2.:atan(y,x);
+}
+
+vec2 xy2pol(vec2 xy){
+    return vec2(atan2(xy.y,xy.x),length(xy));
+}
+
+vec2 pol2xy(vec2 pol){
+    return pol.y*vec2(cos(pol.x),sin(pol.x));
+}
+
+float easeInOutSine(float x){
+    return clamp(-.5*(cos(PI*x)-1.)+.1,.1,1.);
+}
+
+void main(void){
+    vec2 uv=vTexCoord;
+    uv+=vec2(random(uv))*.05;
+    
+    vec4 col=texture2D(u_tex,uv);
+    
+    float gray=easeInOutSine((col.r+col.g+col.b)/3.);
+    
+    gl_FragColor=vec4(vec3(gray),1.);
+}
